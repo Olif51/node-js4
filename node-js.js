e5e99cce5d994bb23786f56d99bbbd9de987c9f3
Olif51 <99129655+Olif51@users.noreply.github.com>
2023-01-24 10:48:47 +0100

    node-js quest

---
+++ node-js.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+
+console.log("Hi, I am" + process.env.MY_NAME);
+console.log("I am from" + process.env.MY_CITY);
+console.log("I barrely speak" + process.env.MY_LANGUAGE);
